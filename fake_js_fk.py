38d2235e555ed9e8366be2aecb23b758ee769aea
ZakariaChekakta <12950797+ZakariaChekakta@users.noreply.github.com>
2022-03-27 22:18:24 +0100

    Update fake_js_fk.py

--- fake_js_fk.py
+++ fake_js_fk.py
@@ -1,28 +1,16 @@
 #!/usr/bin/env python 
 
-import sys  
-
-import copy  
-
+import sys   
 import rospy  
-
 from moveit_msgs.srv import GetPositionFK, GetPositionFKRequest  
-
 from std_msgs.msg import Header  
 
- 
 
 ### Initialize ros node 
 
 rospy.init_node('get_fk', anonymous=True)  
 
- 
- 
-
 ### Create ros service  
-
- 
-
 try:  
 
     moveit_fk=rospy.ServiceProxy('/my_gen3/compute_fk',GetPositionFK)  
@@ -31,32 +19,13 @@ except rospy.ServiceException as e:
 
     rospy.logerror("Service call failed : %s" %e)  
 
- 
-
-### Create and define the request msg 
-
- 
- 
-
-rqst= GetPositionFKRequest()  
-
-rqst.header.frame_id="base_link"  
-
-rqst.fk_link_names=["end_effector_link"]  
-
-rqst.robot_state.joint_state.name=["joint_1", "joint_2", "joint_3", "joint_4", "joint_5", "joint_6"]  
-
-rqst.robot_state.joint_state.position=[]  
-
+### Create and define the request msg (Write your code here)
  
  
  
-
+ 
+ 
 ### Joint State fake data 
-
- 
- 
-
 i=1  
 
 while (i<7):  
@@ -73,9 +42,5 @@ rospy.loginfo(rqst.robot_state.joint_state.position)
  
 
 ### Display the computed FK of the fake joint state 
-
- 
-
 res=moveit_fk(rqst)  
-
 rospy.loginfo(["computed FK: ", res]) 
