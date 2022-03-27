57aa3e24a8fc496d9fd8c14114be8101ed0cb99f
ZakariaChekakta <12950797+ZakariaChekakta@users.noreply.github.com>
2022-03-27 22:20:32 +0100

    Update js_sub.py

--- js_sub.py
+++ js_sub.py
@@ -1,45 +1,30 @@
 #!/usr/bin/env python 
 
- 
 import rospy 
-
 from sensor_msgs.msg import JointState 
 
  
  
-
+### define the subsciber class
 class JointStateSub(object): 
-
     def __init__(self): 
-
         rospy.loginfo("Initalizing node...") 
-
-        self.js_sub = rospy.Subscriber('/my_gen3/joint_states', JointState,self.js_cb, queue_size=1) 
-
- 
- 
-
+        self.js_sub = rospy.Subscriber('/my_gen3/joint_states', JointState,self.js_cb, queue_size=1)  
     def js_cb(self, data): 
-
         self.last_js = data 
-
         rospy.loginfo(data) 
 
  
  
-
+### main function
 if __name__ == '__main__': 
-
-    rospy.init_node('joint_state_node_subscriber') 
-
-    rospy.loginfo("Start joint state subscriber node") 
-
-    JointStateSub().js_cb 
-
+    ### wrtie the main code here
+  
+  
+  
+    
+    ### Spin the rose node
     try: 
-
         rospy.spin() 
-
     except KeyboardInterrupt: 
-
        print("Shutting down") 
